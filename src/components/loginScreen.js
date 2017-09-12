671e436bc5605eb972b7b895684566fe84a954e9
SergiyKistenyuk <sergiy.kistenyuk@gmail.com>
2017-09-12 22:19:15 +0300

    login screen

--- src/components/loginScreen.js
+++ src/components/loginScreen.js
@@ -1,49 +1,98 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationActions } from 'react-navigation';
+import { Button, FormLabel, FormInput, Card, Icon } from 'react-native-elements';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    image: {
+        alignSelf: 'flex-start',
+        width: 137,
+        height: 31,
+        marginTop: 20,
+        marginLeft: 12,
+        marginBottom: 20
+    },
+    header: {
+        fontSize: 20,
+        margin: 10,
+    },
+    signInButton: {
+        marginTop: 20
+    },
+    signUpBlock: {
+        alignSelf: 'flex-start'
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'left',
+        marginLeft: 12
+    },
+    signUpButton: {
+        backgroundColor: 'blue',
+        color: 'white'
+    }
 });
 
 const LoginScreen = ({ navigation, dispatch }) => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Login
-    </Text>
-    <Text style={styles.instructions}>
-      This is great
-    </Text>
-    <Button
-      onPress={() => navigation.dispatch({ type: 'Login' })}
-      title="Log in"
-    />
-    <Button
-      onPress={() =>
-        dispatch(NavigationActions.navigate({ routeName: 'Signup' }))}
-      title="Signup"
-    />
-  </View>
+    <View style={styles.container}>
+        <Image style={styles.image} source={require('../images/logo.png')} ></Image>
+        <Text style={styles.header}>
+            Sign in
+        </Text>        
+        <Card>
+                <FormLabel>Email</FormLabel>
+                <FormInput
+                    placeholder="Email address..."
+                    small
+                    iconLeft
+                    Icon={{name: 'mail'}}
+
+                />
+                <FormLabel>Password</FormLabel>
+                <FormInput
+                    secureTextEntry
+                    placeholder="Password..."
+                    small
+                    iconLeft
+                    Icon={{name: 'lock'}}
+                />
+
+                <Button
+                style={styles.signInButton}
+                backgroundColor="#03A9F4"
+                title="Sign in"
+                onPress={() => navigation.dispatch({ type: 'Login' })}
+                />
+        </Card>
+        <View style={styles.signUpBlock}>
+            <Text style={styles.text}>
+                Dont have an account?
+            </Text>
+            <Button
+                style={styles.signUpButton}
+                onPress={() =>
+                    dispatch(NavigationActions.navigate({ routeName: 'Signup' }))}
+                small
+                title="Sign up for free"
+            />
+        </View>
+    </View>
 );
 
 LoginScreen.propTypes = {
-  navigation: PropTypes.object.isRequired
+    navigation: PropTypes.object.isRequired
 };
 
 LoginScreen.navigationOptions = {
-  title: 'Log In',
+    title: 'Log In',
 };
 
 export default connect()(LoginScreen);
--- src/components/signupScreen.js
+++ src/components/signupScreen.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Card } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        paddingVertical: 20
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
 });
 
 const SignupScreen = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Sign up
-    </Text>
-    <Text style={styles.instructions}>
-      Create an account
-    </Text>
-    <Button
-      onPress={() => navigation.dispatch({ type: 'Login' })}
-      title="Login"
-    />
-  </View>
+    <View style={styles.container}>
+        <Text style={styles.welcome}>
+            Sign up
+        </Text>
+        <Text style={styles.instructions}>
+            Create an account
+        </Text>
+        <Button
+            onPress={() => navigation.dispatch({ type: 'Login' })}
+            title="Sign up for free"
+        />
+    </View>
 );
 
 SignupScreen.propTypes = {
-  navigation: PropTypes.object.isRequired,
+    navigation: PropTypes.object.isRequired,
 };
 
 SignupScreen.navigationOptions = {
-  title: 'Sign Up',
+    title: 'Sign Up',
 };
 
 export default SignupScreen;
