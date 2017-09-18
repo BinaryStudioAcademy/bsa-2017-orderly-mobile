526caa7fb309797db8c4fcd833a12d55796d77ba
Chulanovskyi <skyrocker2013@gmail.com>
2017-09-18 22:15:48 +0300

    feature/Dashboard: test new ssh

--- src/navigators/appNavigator.js
+++ src/navigators/appNavigator.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { addNavigationHelpers, StackNavigator, TabNavigator } from 'react-navigation';
+import {connect} from 'react-redux';
+import {addNavigationHelpers, StackNavigator, TabNavigator} from 'react-navigation';
 import axios from 'axios';
-import LoginScreen from '../components/auth/login/loginScreen';
+
+import AppConfig from '../config';
+import NavigatorService from './navigatorService';
+import Auth from '../components/auth/auth';
 import SignupScreen from '../components/auth/signUp/signupScreen';
+import LoginScreen from '../components/auth/login/loginScreen';
 import Logout from '../components/auth/logout/logout';
 import HomeScreen from '../components/homeScreen/homeScreen';
 import ProfileScreen from '../components/profile/profileScreen';
 import DashboardScreen from '../components/dashboard/dashboardScreen';
 import TableScreen from '../components/dashboard/table/tableScreen';
-import Auth from '../components/auth/auth';
-import AppConfig from '../config';
-import NavigatorService from './navigatorService';
 
-export const DashBoardNavigator = TabNavigator({
-    Table: { screen: TableScreen },
-    Table2: { screen: TableScreen },
+const TablesNavigator = TabNavigator({
+    Table: {screen: TableScreen}
+}, {
+    tabBarPosition: 'bottom',
+});
+
+const DashBoardNavigator = StackNavigator({
+    Dashboard: {screen: DashboardScreen},
+    Tables: {screen: TablesNavigator},
 });
 
 export const AppNavigator = StackNavigator({
-    Login: { screen: LoginScreen },
-    Signup: { screen: SignupScreen },
-    Logout: { screen: Logout },
-    Home: { screen: HomeScreen },
-    Profile: { screen: ProfileScreen },
-    Dashboard: { screen: DashBoardNavigator }
+    Login: {screen: LoginScreen},
+    Signup: {screen: SignupScreen},
+    Logout: {screen: Logout},
+    Home: {screen: HomeScreen},
+    Profile: {screen: ProfileScreen},
+    Dashboard: {screen: DashBoardNavigator}
 });
 
-const AppWithNavigationState = ({ dispatch, nav, dashboard }) => (
+const AppWithNavigationState = ({dispatch, nav, dashboard}) => (
     <AppNavigator
         navigation={addNavigationHelpers({
             dispatch,
@@ -51,7 +58,7 @@ const mapStateToProps = (state) => ({
 
 export default connect(mapStateToProps)(AppWithNavigationState);
 
-(function() {
+(function () {
     axios.defaults.baseURL = AppConfig.host;
 
     Auth.getToken().then((token) => {
